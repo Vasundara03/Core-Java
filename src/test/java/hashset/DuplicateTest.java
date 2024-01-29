64287fdd8964939a16f6490a493db84983119e23
Vasundara03 <95501368+Vasundara03@users.noreply.github.com>
2024-01-29 10:30:50 -0600

    added hashset test cases

--- src/test/java/hashset/DuplicateTest.java
+++ src/test/java/hashset/DuplicateTest.java
@@ -1,2 +1,24 @@
-package hashset;public class DuplicateTest {
+package hashset;
+
+import hashset.DuplicateChar;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class DuplicateTest extends TestCase {
+
+    DuplicateChar val = new DuplicateChar();
+    public void testDuplicate(){
+
+        //true case
+        Assert.assertEquals("alis",val.noDuplicate("alias"));
+        Assert.assertEquals("sai",val.noDuplicate("saiS"));
+
+        //false case
+        Assert.assertNotSame("ais",val.noDuplicate("sai"));
+
+        //null case
+        Assert.assertNotSame(" ",val.noDuplicate(" "));
+        Assert.assertNotSame(" ",val.noDuplicate(null));
+
+    }
 }
--- src/test/java/hashset/SetTest.java
+++ src/test/java/hashset/SetTest.java
@@ -1,2 +1,39 @@
-package hashset;public class SetTest {
+package hashset;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import junit.framework.TestCase;
+
+import hashset.SetCreate;
+import junit.framework.Assert;
+
+
+import java.util.*;
+
+public class SetTest extends TestCase {
+
+    HashSet<String> hset = new HashSet<>();
+    SetCreate sc = new SetCreate();
+    Iterator<String> iterateFunIterator = sc.iterateFun(hset);
+    public void testLoop() {
+
+        String[] elements = {"a","c","b","A"};
+        hset.addAll(Arrays.asList(elements));
+        String[] expectedListOutput = hset.toArray(new String[0]);
+        Iterator<String> iterate = hset.iterator();
+        for (int i = 0; i < hset.size(); i++) {
+            //for loop
+            Assert.assertEquals(expectedListOutput[i], sc.iterateUsingForLoop(hset)[i]);
+        }
+        int i=0;
+        for(String val:hset){
+            Assert.assertEquals(val,sc.iterateUsingForLoop(hset)[i++]);
+        }
+        Iterator<String> iterateFunIterator = sc.iterateFun(hset);
+        while(iterate.hasNext()){
+            Assert.assertEquals(iterate.next(),iterateFunIterator.next());
+        }
+
+    }
 }
