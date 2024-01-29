4136f53e314ecef08604db0fcb50c596b7c87ce8
Vasundara03 <95501368+Vasundara03@users.noreply.github.com>
2024-01-29 10:25:24 -0600

    added map files

--- src/main/java/maps/ReturnStudentObject.java
+++ src/main/java/maps/ReturnStudentObject.java
@@ -1,2 +1,21 @@
-package maps;public class ReturnStudentObject {
+package maps;
+
+import arraylist.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReturnStudentObject {
+
+    public static HashMap<String, Student> studentObjects() {
+        HashMap<String, Student> studentobj = new HashMap<>();
+        for (Map.Entry<String, Student> entry : studentobj.entrySet()) {
+            Student student = entry.getValue();
+            studentobj.put(student.getFirstName(),student);
+        }
+        return studentobj;
+    }
+    public static Student returnStudentObj(HashMap<String,Student> studentobj, String name){
+        return studentobj.get(name);
+    }
 }
--- src/main/java/maps/Student.java
+++ src/main/java/maps/Student.java
@@ -1,2 +1,28 @@
-package maps;public class Student {
+package maps;
+import java.util.ArrayList;
+
+public class Student {
+
+    public String firstName;
+    public String lastName;
+    public double gpa;
+
+    public Student(String firstName, String lastName, double gpa) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gpa = gpa;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
 }
+
