c1e7ed72e412714f23eb2a5ba453e543b27259dd
Vasundara03 <95501368+Vasundara03@users.noreply.github.com>
2024-02-09 14:21:03 -0600

    added test

--- src/test/java/threadtest/OddEvenPrinterTest.java
+++ src/test/java/threadtest/OddEvenPrinterTest.java
@@ -1,2 +1,16 @@
-package threadtest;public class OddEvenPrinterTest {
+package threadtest;
+
+import org.junit.Test;
+import threads.OddEvenPrinter;
+
+public class OddEvenPrinterTest {
+
+    @Test
+    public void testOddEvenPrinting() throws InterruptedException {
+        System.out.println("Test Case:");
+        OddEvenPrinter printer = new OddEvenPrinter();
+        printer.printOddEvenNumbers();
+        // To ensure that test completes after threads execution
+        Thread.sleep(1000); // Wait for 1 second
+    }
 }
